c5b09c56a55a14b58be9e0dfd6c33b64fb75181d
AbrahaKahsay <abrahak81@gmail.com>
2022-12-13 15:44:09 +0300

    Update two_sum.js

--- two_sum.js
+++ two_sum.js
@@ -15,3 +15,20 @@ Example 3:
 
 Input: nums = [3,3], target = 6
 Output: [0,1]
+_________________________________________
+
+// my solution
+var twoSum = function(nums, target) {
+    for (let i=0; i<nums.length; i++){
+        for(let j=1; j<nums.length; j++){
+            if(nums[i]+nums[j] === target && i != j){
+                return [i, j]
+            }
+        }
+      
+        // if (nums[i]+nums[i+1]===target){
+        //     return [i, i+1]
+        // }
+    }
+   
+};
