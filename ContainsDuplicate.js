f4d65ac3ab3b7d759f39452848d68b0740c44ee3
AbrahaKahsay <abrahak81@gmail.com>
2022-12-14 10:27:35 +0300

    Comment out problem descriptions

--- ContainsDuplicate.js
+++ ContainsDuplicate.js
@@ -1,26 +1,26 @@
-# Challenge 1. Contains Duplicate
-===================================
+// # Challenge 1. Contains Duplicate
+// ===================================
 
-Given an integer array nums, return true if any value appears at least twice in the array,
-and return false if every element is distinct.
+// Given an integer array nums, return true if any value appears at least twice in the array,
+// and return false if every element is distinct.
 
-Example 1:
+// Example 1:
 
-Input: nums = [1,2,3,1]
-Output: true
+// Input: nums = [1,2,3,1]
+// Output: true
 
-Example 2:
+// Example 2:
 
-Input: nums = [1,2,3,4]
-Output: false
+// Input: nums = [1,2,3,4]
+// Output: false
 
-Example 3:
+// Example 3:
 
-Input: nums = [1,1,1,3,3,4,3,2,4,2]
-Output: true
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
 
-# challenge solved using #set data structure as set will gives a unique values. 
-set is not array and we should convert it using Array.from() method or array destructuring. [...new Set(nums)]
+// # challenge solved using #set data structure as set will gives a unique values. 
+// set is not array and we should convert it using Array.from() method or array destructuring. [...new Set(nums)]
 
 var containsDuplicate = function(nums) {
     let uniqueNums = Array.from(new Set(nums))
