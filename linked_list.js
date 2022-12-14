49e3f52f6e3ec2f28cc0cf388c4279ba2192ff07
AbrahaKahsay <abrahak81@gmail.com>
2022-12-14 10:28:38 +0300

    Add Node class

---
+++ linked_list.js
@@ -0,0 +1,13 @@
+// LinkedList is the dynamic data structure, as we can add or remove elements at ease, 
+// and it can even grow as needed. Just like arrays, linked lists store elements sequentially, 
+// but don’t store the elements contiguously like an array. 
+
+// User defined class node
+class Node {
+	// constructor
+	constructor(element)
+	{
+		this.element = element;
+		this.next = null
+	}
+}
