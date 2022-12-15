844c86826674a26ec9de27e24b2c67ee1274d9bd
AbrahaKahsay <abrahak81@gmail.com>
2022-12-15 17:25:22 +0300

    Create the LinkedList class

--- linked_list.js
+++ linked_list.js
@@ -1,7 +1,21 @@
-// create a function for a new node object
+// create a function for a new node object 
 class Node {
     constructor(value, next_node = null){
-        this.value = value
-        this.next_node = next_node
+        this.value = value;
+        this.next_node = next_node;
     }
 }
+
+// create the LinkedList class
+class LinkedList {
+    constructor() {
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+}
+
+
+
+let node1 = new Node("hello")
+console.log(node1)
