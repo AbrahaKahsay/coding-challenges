55eac7b4a108497c18d4903b071e65a7fa2f5d93
AbrahaKahsay <abrahak81@gmail.com>
2022-12-17 14:21:10 +0300

    Add prepend and append methods to Linkedlist

--- linked_list.js
+++ linked_list.js
@@ -1,8 +1,8 @@
 // create a function for a new node object 
 class Node {
-    constructor(value, next_node = null){
+    constructor(value, next = null){
         this.value = value;
-        this.next_node = next_node;
+        this.next = next;
     }
 }
 
@@ -11,11 +11,33 @@ class LinkedList {
     constructor() {
         this.head = null;
         this.tail = null;
-        this.length = 0;
+        // this.length = 0;
+    }
+
+        // the prepend method adds a LinkedListNode object
+        // the begining of our LinkedList.
+    prepend(value){
+        // make new node to be a head
+        const newNode = new Node(value, this.head);
+        this.head = newNode;
+        // if there is no tail yet let's make new node a tail
+        if(this.tail === null) { //this is same as !this.tail
+        this.tail = newNode
+        }
+        return this;
+    }
+
+    append(value){   //this method creates a Node object to the end of our linked list.
+        const newNode = new Node(value);
+        // If there is no head yet let's make new node a head.
+        if (this.head === null){
+            this.head = newNode;
+            this.tail = newNode;
+        }
+        return this;
     }
 }
 
-
-
-let node1 = new Node("hello")
-console.log(node1)
+let ll = new LinkedList();
+console.log(ll.append(30));
+// console.log(ll)
