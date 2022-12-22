469fb9886b878986663cfe3b34c0a5cd37b005a7
AbrahaKahsay <abrahak81@gmail.com>
2022-12-22 10:38:45 +0300

    Add challenges to respective folders

---
+++ leetCode/contain_duplicates.js
@@ -0,0 +1,33 @@
+// # Challenge 1. Contains Duplicate
+// ===================================
+
+// Given an integer array nums, return true if any value appears at least twice in the array,
+// and return false if every element is distinct.
+
+// Example 1:
+
+// Input: nums = [1,2,3,1]
+// Output: true
+
+// Example 2:
+
+// Input: nums = [1,2,3,4]
+// Output: false
+
+// Example 3:
+
+// Input: nums = [1,1,1,3,3,4,3,2,4,2]
+// Output: true
+
+// # challenge solved using #set data structure as set will gives a unique values. 
+// set is not array and we should convert it using Array.from() method or array destructuring. [...new Set(nums)]
+
+var containsDuplicate = function(nums) {
+    let uniqueNums = Array.from(new Set(nums))
+    if(nums.length > uniqueNums.length){
+        return true
+    } 
+    else {
+        return false
+    }
+};
