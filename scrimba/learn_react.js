5146b9b411ceff46a337b025b75b283999bbd8cd
AbrahaKahsay <abrahak81@gmail.com>
2022-12-27 10:53:26 +0300

    Add and append an html element to dom with imperative way

--- scrimba/learn_react.js
+++ scrimba/learn_react.js
@@ -2,3 +2,18 @@
 // React is declerative. you need to tell it what should be done and it handles the other things.
 // The opposite for declerative is imperative where you describe how the code should be excuted like
 // the way we do in vanilla JavaScript.
+
+
+// Imperative way
+// Challenge - recreate the above line of code in vanilla JS by creating and
+// appending an h1 to our div#root (without using innerHTML).
+
+// - Create a new h1 element
+// - Give it some textContent
+// - Give it a class name of "header"
+// - append it as a child of the div#root
+
+const h1 = document.createElement("h1");
+h1.textContent = "I am doing imperative way of writing JavaScript";
+h1.className = "header";
+document.getElementById("root").appendChild(h1)
