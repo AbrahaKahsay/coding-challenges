1a5715f5e05ffdfc46cb364762870b55c2d38d6f
AbrahaKahsay <abrahak81@gmail.com>
2022-12-27 10:50:34 +0300

    Add a learn_react file

---
+++ scrimba/learn_react.js
@@ -0,0 +1,4 @@
+// React is composable. we can produce custom components that can be reused
+// React is declerative. you need to tell it what should be done and it handles the other things.
+// The opposite for declerative is imperative where you describe how the code should be excuted like
+// the way we do in vanilla JavaScript.
