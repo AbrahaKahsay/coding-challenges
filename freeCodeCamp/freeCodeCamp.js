8b684913ed7c33edf57ab10801e0a0629aeb5e4a
Abraha Kahsay <abrahak81@gmail.com>
2022-12-30 21:44:58 +0300

    Update freeCodeCamp.js

--- freeCodeCamp/freeCodeCamp.js
+++ freeCodeCamp/freeCodeCamp.js
@@ -235,3 +235,16 @@ function bouncer(arr) {
 }
 
 bouncer([7, "ate", "", false, 9]);
+
+
+// Use the sort method in the alphabeticalOrder function to sort the elements of arr in alphabetical order.
+// The function should return the sorted array.
+
+function alphabeticalOrder(arr) {
+  // Only change code below this line
+
+  return arr.sort()
+  // Only change code above this line
+}
+
+alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
