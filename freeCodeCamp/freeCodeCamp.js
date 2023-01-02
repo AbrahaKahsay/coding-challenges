e47106aa7d653018552bede6fb4185089d2b0f75
Abraha Kahsay <abrahak81@gmail.com>
2023-01-02 23:54:10 +0300

    Update freeCodeCamp.js

--- freeCodeCamp/freeCodeCamp.js
+++ freeCodeCamp/freeCodeCamp.js
@@ -248,3 +248,17 @@ function alphabeticalOrder(arr) {
 }
 
 alphabeticalOrder(["a", "d", "c", "a", "z", "g"]);
+
+// Use the sort method in the nonMutatingSort function to sort the elements of an array in ascending order. 
+// The function should return a new array, and not mutate the globalArray variable.
+const globalArray = [5, 6, 3, 2, 9];
+
+function nonMutatingSort(arr) {
+  // Only change code below this line
+
+
+  // Only change code above this line
+}
+
+nonMutatingSort(globalArray);
+
