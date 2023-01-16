7cc1727855b852011c5ef98242d882191e9b945d
AbrahaKahsay <abrahak81@gmail.com>
2023-01-16 23:58:22 +0300

    Add switch challenge

--- hackerank/switch.js
+++ hackerank/switch.js
@@ -19,3 +19,27 @@ function getLetter(s) {
     
     return letter;
 }
+
+
+// Better solution
+
+// function getLetter(s) {
+//     let letter;
+//     // Write your code here
+ 
+//   switch (true) {
+//     case 'aeiou'.includes(s[0]):
+//       letter = "A"
+//       break;
+//     case 'bcdfg'.includes(s[0]):
+//       letter = "B"
+//       break;
+//     case 'hjklm'.includes(s[0]):
+//       letter = "C"
+//       break;
+//     case 'npqrstuwxyz'.includes(s[0]):
+//       letter = "D"
+//       break;
+//   }
+//   return letter
+// }
