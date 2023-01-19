4a7c89135f6db6e9be183572d1604d867982e139
AbrahaKahsay <abrahak81@gmail.com>
2023-01-19 13:41:52 +0300

    Add class vs intance challenge

---
+++ hackerank/day30/d4-class-vs-instance.js
@@ -0,0 +1,26 @@
+class Person{
+    constructor(initialAge){
+       if(initialAge<0){
+         this.age = 0;
+         console.log("Age is not valid, setting age to 0.")
+       } else{
+         this.age = initialAge;  
+       }    
+    }
+    
+    amIOld(){
+        if (this.age<13){
+             console.log("You are young.")
+         }
+         else if(this.age>=13 && this.age<18){
+             console.log("You are a teenager.")
+         }else{
+             console.log("You are old.")
+         }
+        };
+        
+    yearPasses(){
+        this.age++;
+    };
+ }
+ 
