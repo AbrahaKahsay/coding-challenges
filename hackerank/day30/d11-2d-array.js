80ba2c33beda94bd76238922a2494a3db09bc989
AbrahaKahsay <abrahak81@gmail.com>
2023-01-24 21:58:24 +0300

    2d-array

---
+++ hackerank/day30/d11-2d-array.js
@@ -0,0 +1,8 @@
+function main() {
+
+    let arr = Array(6);
+
+    for (let i = 0; i < 6; i++) {
+        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    }
+}
