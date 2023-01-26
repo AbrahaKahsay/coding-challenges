b583018827aa6970f476d4c5e49499051980916b
AbrahaKahsay <abrahak81@gmail.com>
2023-01-26 22:04:05 +0300

    Add binary numbers

---
+++ hackerank/day30/d10-binary-numbers.js
@@ -0,0 +1,19 @@
+
+function main() {
+    const n = parseInt(readLine().trim(), 10);
+    // convert n into binary
+    // loop through the array to check if the next
+    // element is same with the previous and count
+    let arr = n.toString(2);
+    let newArr = arr.split('')
+    let count = 0;
+    for(let i=0; i<newArr.length; i++){
+        // console.log(newArr[i])
+        if(newArr[i] == newArr[i+1]){
+            count++;
+        } else {
+            count = 1
+        }
+    }
+    console.log(count);
+}
