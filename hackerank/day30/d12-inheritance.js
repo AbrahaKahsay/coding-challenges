640cca6b0dae0a37e328a2f971fda1e76c9e0a1e
AbrahaKahsay <abrahak81@gmail.com>
2023-01-28 22:50:40 +0300

    Add inheritance challenge

---
+++ hackerank/day30/d12-inheritance.js
@@ -0,0 +1,35 @@
+class Student extends Person {
+    /*	
+    *   Class Constructor
+    *   
+    *   @param firstName - A string denoting the Person's first name.
+    *   @param lastName - A string denoting the Person's last name.
+    *   @param id - An integer denoting the Person's ID number.
+    *   @param scores - An array of integers denoting the Person's test scores.
+    */
+    // Write your constructor here
+
+    /*	
+    *   Method Name: calculate
+    *   @return A character denoting the grade.
+    */
+    // Write your method here
+    
+}
+
+function main() {
+    let firstName = read()
+    let lastName = read()
+    let id = +read()
+    let numScores = +read()
+    let testScores = new Array(numScores)
+    
+    for (var i = 0; i < numScores; i++) {
+        testScores[i] = +read()  
+    }
+
+    let s = new Student(firstName, lastName, id, testScores)
+    s.printPerson()
+    s.calculate()
+    console.log('Grade: ' + s.calculate())
+}
