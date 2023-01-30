9ccd55034008220b2f3704fb40c5fe3b2cf552eb
AbrahaKahsay <abrahak81@gmail.com>
2023-01-30 16:29:26 +0300

    Complete challenge

--- hackerank/day30/d11-2d-array.js
+++ hackerank/day30/d11-2d-array.js
@@ -1,8 +1,27 @@
-function main() {
+let arr = [
+    [1, 1, 1, 0, 0, 0],
+    [0, 1, 0, 0, 0, 0],
+    [1, 1, 1, 0, 0, 0],
+    [0, 0, 2, 4, 4, 0],
+    [0, 0, 0, 2, 0, 0],
+    [0, 0, 1, 2, 4, 0]
+    ]
 
-    let arr = Array(6);
+function sum_hourglass(arr) {
 
-    for (let i = 0; i < 6; i++) {
-        arr[i] = readLine().replace(/\s+$/g, '').split(' ').map(arrTemp => parseInt(arrTemp, 10));
+    let maxSum = -63;
+
+    for (let i=0; i < 4; i++){
+        for(let j=0; j<4; j++){
+            let top = arr[i][j]+ arr[i][j+1]+arr[i][j+2];
+            let middle = arr[i+1][j+1];
+            let bottom = arr[i+2][j]+ arr[i+2][j+1]+arr[i+2][j+2];
+            let current_sum = top + middle+bottom
+            
+            maxSum = Math.max(current_sum, maxSum)
+        }
     }
+    console.log(maxSum)
 }
+
+sum_hourglass(arr);
