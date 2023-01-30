5dcc372ed654a395a040c74873aeb72f934cf275
AbrahaKahsay <abrahak81@gmail.com>
2023-01-30 16:31:02 +0300

    Modify for the length of arrays

--- hackerank/day30/d11-2d-array.js
+++ hackerank/day30/d11-2d-array.js
@@ -11,8 +11,8 @@ function sum_hourglass(arr) {
 
     let maxSum = -63;
 
-    for (let i=0; i < 4; i++){
-        for(let j=0; j<4; j++){
+    for (let i=0; i < arr[i].length-2; i++){
+        for(let j=0; j<arr[j].length-2; j++){
             let top = arr[i][j]+ arr[i][j+1]+arr[i][j+2];
             let middle = arr[i+1][j+1];
             let bottom = arr[i+2][j]+ arr[i+2][j+1]+arr[i+2][j+2];
