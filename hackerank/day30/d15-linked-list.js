aa9de094388da3f4d9010e669150c1edb335569c
AbrahaKahsay <abrahak81@gmail.com>
2023-01-31 23:09:10 +0300

    Add linked list challenge

---
+++ hackerank/day30/d15-linked-list.js
@@ -0,0 +1,14 @@
+function Node(data){
+    this.data=data;
+    this.next=null;
+}
+
+function Solution(){
+
+	this.insert=function(head,data){
+          //complete this method
+    };
+
+	this.display=function(head){
+    }
+}
