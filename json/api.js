4e577f0216f677217c8e826f850c9656ec0c60af
AbrahaKahsay <abrahak81@gmail.com>
2023-02-03 23:05:30 +0300

    using axios to fetch api

--- json/api.js
+++ json/api.js
@@ -1,7 +1,18 @@
+// using fetch
+
 fetch('https://jsonplaceholder.typicode.com/posts')
   .then(response => response.json())
   .then(data => {
     console.log(data);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+//  using axios
+axios.get('https://jsonplaceholder.typicode.com/posts')
+  .then(response => {
+    console.log(response.data);
   })
   .catch(error => {
     console.error(error);
