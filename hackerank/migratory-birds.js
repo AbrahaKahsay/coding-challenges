eb2f18af0bb3f01fae8dc1d80221f6d4314d0796
AbrahaKahsay <abrahak81@gmail.com>
2023-02-09 22:17:16 +0300

    Add migratory bird challenge

---
+++ hackerank/migratory-birds.js
@@ -0,0 +1,15 @@
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function migratoryBirds(arr) {
+    // Write your code here
+
+}
