08b67dd4cbc05ab591dff0216cd4f3569456e75c
AbrahaKahsay <abrahak81@gmail.com>
2023-03-06 22:57:46 +0300

    Get some practice with objects DSA

---
+++ Algos/objects.js
@@ -0,0 +1,13 @@
+// Object Property Shorthand:
+
+// Write a function that takes in two parameters, 'firstName' and 'lastName', 
+// and returns an object with the properties firstName and lastName.
+
+function fullName(firstName, lastName){
+    return {
+        firstName: firstName,
+        lastNAme: lastName
+    }
+}
+
+console.log(fullName("Abraha", "Kashay"))
