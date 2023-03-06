1212c4cee0c6ab4c78e51b2ed34ba9aa057b9ff2
AbrahaKahsay <abrahak81@gmail.com>
2023-03-06 23:11:17 +0300

    Get some practice with objects DSA

--- Algos/objects.js
+++ Algos/objects.js
@@ -1,4 +1,4 @@
-// Object Property Shorthand:
+// 1. Object Property Shorthand:
 
 // Write a function that takes in two parameters, 'firstName' and 'lastName', 
 // and returns an object with the properties firstName and lastName.
@@ -11,3 +11,19 @@ function fullName(firstName, lastName){
 }
 
 console.log(fullName("Abraha", "Kashay"))
+
+// 2. Object Method Properties:
+
+// Write a function that takes in a string parameter and returns an object 
+// with two methods: toUpperCase and toLowerCase that return the uppercase 
+// and lowercase versions of the string, respectively.
+
+function letterCases(string){
+    return{
+        toUpperCase: string.toUpperCase()
+        ,
+        toLowerCase: string.toLowerCase()
+    }
+}
+
+console.log(letterCases("Abraha"))
