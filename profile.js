3c7a8bce65ca2ade9136d65cf40acb3946e94feb
AbrahaKahsay <abrahak81@gmail.com>
2023-03-14 23:46:39 +0300

    Add profile bg for linked in

---
+++ profile.js
@@ -0,0 +1,26 @@
+
+const me = {
+    name: "Abraha",
+    jobTitle: "Web Developer",
+    skills: ["HTML", "CSS", "JavaScript", "React", "PostgreSQL", "Ruby", "Rails"],
+    introduction: `Hi, I'm ${this.name}, a ${this.jobTitle} with expertise in 
+        ${this.skills.join(", ")}. I am passionate about creating beautiful web
+        experiences that engage and delight users. Let's build something great together!`
+};
+console.log(me.introduction);
+
+
+
+
+
+
+
+
+
+  
+
+
+
+
+
+
