bd382e176e867f0e1669a9060c7d75ac642035fd
AbrahaKahsay <abrahak81@gmail.com>
2023-03-23 22:45:26 +0300

    Add insertion sort

--- profile.js
+++ profile.js
@@ -1,13 +1,13 @@
 
-const me = {
-    name: "Abraha",
-    jobTitle: "Web Developer",
-    skills: ["HTML", "CSS", "JavaScript", "React", "PostgreSQL", "Ruby", "Rails"],
-    introduction: `Hi, I'm ${this.name}, a ${this.jobTitle} with expertise in 
-        ${this.skills.join(", ")}. I am passionate about creating beautiful web
-        experiences that engage and delight users. Let's build something great together!`
-};
-console.log(me.introduction);
+                const me = {
+                    name: "Abraha",
+                    title: "Web Developer",
+                    skills: ["HTML", "CSS", "JavaScript", "React", "PostgreSQL", "Ruby", "Rails"],
+                    introduction: `Hi, I'm ${this.name}, a ${this.title} with expertise in 
+                        ${this.skills.join(", ")}. I am passionate about creating beautiful web
+                        experiences that engage and delight users. Let's build something great together!`
+                };
+                        console.log(me.introduction);
 
 
 
