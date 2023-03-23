076f844277ddfd52f475e7f10d1ea6b3b75f88b7
AbrahaKahsay <abrahak81@gmail.com>
2023-03-23 23:15:54 +0300

    Add insertion sort

--- Algos/insertion_sort.js
+++ Algos/insertion_sort.js
@@ -12,3 +12,18 @@ function insertionSort(arr) {
   
   console.log(insertionSort([3, 1, 7, 5, 2])); // Output: [1, 2, 3, 5, 7]
   
+
+// or
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+      let j = i - 1;
+      let temp = arr[i];
+      while (j >= 0 && arr[j] > temp) {
+        arr[j + 1] = arr[j];
+        j--;
+      }
+      arr[j + 1] = temp;
+    }
+    return arr;
+  }
+  
