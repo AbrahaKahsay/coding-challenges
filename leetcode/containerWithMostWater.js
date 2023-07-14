29423b00a13afd3755eda2d08d591d3e0a51d5a4
Abraha Kahsay <abrahak81@gmail.com>
2023-07-14 16:54:34 +0300

    Update containerWithMostWater.js

--- leetcode/containerWithMostWater.js
+++ leetcode/containerWithMostWater.js
@@ -2,6 +2,10 @@
  * @param {number[]} height
  * @return {number}
  */
+
+// This problem is of greedy algorithm type. we should approach from the maximum index difference which 
+// we should use two pointers at the start and end until we get the desired output. this helps to optimize the 
+// time complexity with the problem of using 2 for loops.
 var maxArea = function(height) {
     let maxA = 0;
     let i = 0;
