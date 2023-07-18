0c6b031018b242b96dbb4736821e67c29a29950b
Abraha Kahsay <abrahak81@gmail.com>
2023-07-19 01:45:13 +0300

    Update linkedListCycle.js

--- leetcode/linkedListCycle.js
+++ leetcode/linkedListCycle.js
@@ -17,9 +17,11 @@ var hasCycle = function(head) {
     while(fast !== null && fast.next !== null){
         slow = slow.next;
         fast = fast.next.next
-    }
-    if(slow==fast){
-        return true
-    }
+
+        if(slow === fast){
+            return true
+        }
+      }
+    
     return false
 };
